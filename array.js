0437d8e865da486ddb34cbed2de8a87b2cb244c2
Sajid Mehmood <sajidroxx@gmail.com>
2018-07-12 14:04:02 +0500

    New array

--- array.js
+++ array.js
@@ -6,30 +6,30 @@ num1=num.map(function(num){
 })
 num2=[17,18,19,10,20];
 num3=num1.concat(num2);
-(num3);
+console.log(num3);
 //2nd function
 
 var data=new Array();
  data[0]=new Array(1,2,3,4,5,6,7);
  data[1]=new Array("Sajid","imtiaz","Umer","Ali","Ahmed","Saqib","Usama");
 
- ("My Roll Number is "+data[0][0]+". my name is "+data[1][0]+".");
- ("My Roll Number is "+data[0][1]+". my name is "+data[1][1]+".");
- ("My Roll Number is "+data[0][2]+". my name is "+data[1][2]+".");
- ("My Roll Number is "+data[0][3]+". my name is "+data[1][3]+".");
- ("My Roll Number is "+data[0][4]+". my name is "+data[1][4]+".");
- ("My Roll Number is "+data[0][5]+". my name is "+data[1][5]+".");
- ("My Roll Number is "+data[0][6]+". my name is "+data[1][6]+".");
+ console.log("My Roll Number is "+data[0][0]+". my name is "+data[1][0]+".");
+ console.log("My Roll Number is "+data[0][1]+". my name is "+data[1][1]+".");
+ console.log("My Roll Number is "+data[0][2]+". my name is "+data[1][2]+".");
+ console.log("My Roll Number is "+data[0][3]+". my name is "+data[1][3]+".");
+ console.log("My Roll Number is "+data[0][4]+". my name is "+data[1][4]+".");
+ console.log("My Roll Number is "+data[0][5]+". my name is "+data[1][5]+".");
+ console.log("My Roll Number is "+data[0][6]+". my name is "+data[1][6]+".");
 
 //3rd function
 var info=[["Saif","Alvi","Umer"],[1,2,3],["10th","11th","12th"]];
-("My name is "+info[0][0]+". My roll Number is "+info[1][0]
+console.log("My name is "+info[0][0]+". My roll Number is "+info[1][0]
 +" and I study in Class "+info[2][0]+".");
 
-("My name is "+info[0][1]+". My roll Number is "+info[1][1]
+console.log("My name is "+info[0][1]+". My roll Number is "+info[1][1]
 +" and I study in Class "+info[2][1]+".");
 
-("My name is "+info[0][2]+". My roll Number is "+info[1][2]
+console.log("My name is "+info[0][2]+". My roll Number is "+info[1][2]
 +" and I study in Class "+info[2][2]+".");
 
 
